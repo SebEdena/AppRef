89db89bb6a9aa13ffaef52faecc0fcd4764cf522
Mathieu VALENTIN <mathieu.valentin@etu.parisdescartes.fr>
2017-03-01 12:16:11 +0100

    MaJ utilisateurs

--- ProjetBLTi/src/utilisateurs/Programmeur.java
+++ ProjetBLTi/src/utilisateurs/Programmeur.java
@@ -4,8 +4,9 @@ public class Programmeur extends Utilisateur {
 
 	private String ftp;
 	
-	public Programmeur(String name, String pwd) {
+	public Programmeur(String name, String pwd, String ftp) {
 		super(name, pwd);
+		this.ftp = ftp;
 	}
 
 }
