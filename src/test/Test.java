34f095e0105f4c64c1c8e6a2b9441883c71f7683
Sebastien VIGUIER <viguier.sebastien@orange.fr>
2017-03-08 11:36:56 +0100

    maj du test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,29 @@
 package test;
 
-public class Test {
+import java.util.List;
 
+import fileUtil.XMLReader;
+import utilisateurs.Amateur;
+import utilisateurs.Programmeur;
+import utilisateurs.Utilisateur;
+
+public class Test {
+ 
+	public static void main(String[] args) {
+		List<String> s = XMLReader.read("./data.xml");
+		System.out.println(s);
+		Utilisateur u = null;
+		for(String str : s){
+			String[] data = str.split(";");
+			switch(data[0]){
+			case "amateur" : 
+				u = new Amateur(data);
+				break;
+			case "prog" : 
+				u = new Programmeur(data);
+				break;
+			}
+			System.out.println(u);
+		}
+	}
 }
